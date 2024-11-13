396e5302ed68466d6186ac48bb225a721e089303
unknown12qw <motivaterthe@gmail.com>
2024-11-13 17:32:58 +0530

    Update bot.php

--- apple/bot.php
+++ apple/bot.php
@@ -10,7 +10,7 @@ if($message == "/start") {
 }
 
 function sendMessage($chat_id, $message) {
-    $apiToken = "7930320812:AAH264MiuNQHIF7K18lb7Cf7R2eZlfKK1fw";
+    $apiToken = "7678741165:AAEDHjIvw5naozYowOQSnAyD1TatG8LeL28";
     $url = "https://api.telegram.org/bot$apiToken/sendMessage?chat_id=$chat_id&text=".urlencode($message);
     file_get_contents($url);
 }
